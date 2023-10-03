a85da83b5dd1aa67254975b7de495498d59f973f
Stian Knudsen <dislabled@gmail.com>
2023-10-03 12:25:38 +0200

    custom westermo scrapli template

---
+++ westermo/weos/async_driver.py
@@ -0,0 +1,38 @@
+"""scrapli_community.westermo.weos.async_driver"""
+from scrapli.driver import AsyncNetworkDriver
+
+
+async def default_async_on_open(conn: AsyncNetworkDriver) -> None:
+    """
+    Async westermo_weos default on_open callable
+
+    Args:
+        conn: NetworkDriver object
+
+    Returns:
+        N/A
+
+    Raises:
+        N/A
+
+    """
+    await conn.acquire_priv(desired_priv=conn.default_desired_privilege_level)
+
+
+async def default_async_on_close(conn: AsyncNetworkDriver) -> None:
+    """
+    Async westermo_weos default on_close callable
+
+    Args:
+        conn: NetworkDriver object
+
+    Returns:
+        N/A
+
+    Raises:
+        N/A
+
+    """
+    await conn.acquire_priv(desired_priv=conn.default_desired_privilege_level)
+    conn.channel.write(channel_input="logout")
+    conn.channel.send_return()
